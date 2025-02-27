76a3c61a392225cccb7b47b1abf35854e3d3c3bc
Source-Code-007 <utsho926@gmail.com>
2025-02-27 23:25:07 +0600

    doctor service logic updated

--- src/app/module/doctor/doctor.service.ts
+++ src/app/module/doctor/doctor.service.ts
@@ -48,6 +48,8 @@ const getDoctorByDoctorCode = async (id: string) => {
   return doctor
 }
 
+
+
 // TODO: need to handle workingExperiences and medicalSpecialties for update doctor
 const updateDoctorById = async (
   id: string,
