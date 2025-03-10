d85da61f7212707ce1be7891c50afeb4138da2a9
Source-Code-007 <utsho926@gmail.com>
2025-03-10 23:29:42 +0600

    added filtering by date in get all appointments

--- src/app/module/appointment/appointment.service.ts
+++ src/app/module/appointment/appointment.service.ts
@@ -6,19 +6,23 @@ import QueryBuilder from '../../builder/QueryBuilder'
 
 const getAllAppointments = async (query: Record<string, unknown>) => {
   const dateFilter: any = {}
-  // if (query.date) {
-  //   const date = new Date(query.date as string)
-  //   const nextDay = new Date(date)
-  //   nextDay.setDate(date.getDate() + 1)
+  if (query.date) {
+    const date = new Date(query.date as string)
+    const nextDay = new Date(date)
 
-  //   dateFilter.schedule = {
-  //     $gte: date.toISOString(),
-  //     $lt: nextDay.toISOString(),
-  //   }
-  // }
-  console.log(dateFilter, query.date, 'dateFilter')
+    nextDay.setDate(date.getDate() + 1)
+
+    dateFilter.schedule = {
+      $gte: date.toISOString(),
+      $lt: nextDay.toISOString(),
+    }
+  }
+
+  delete query.date
+
   const appointmentQuery = new QueryBuilder(Appointment.find(), {
     ...query,
+    ...dateFilter,
     sort: `${query.sort}`,
   })
     .filterQuery()
