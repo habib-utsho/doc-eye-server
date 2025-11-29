d0e74216d0f4948dbd2163f1e83dbf9a43115c41
Source-Code-007 <utsho926@gmail.com>
2025-11-29 17:49:02 +0600

    nodemailer security updated for production build

--- dist/app/utils/sendEmail.js
+++ dist/app/utils/sendEmail.js
@@ -18,11 +18,15 @@ const sendEmail = (payload) => __awaiter(void 0, void 0, void 0, function* () {
     const transporter = nodemailer_1.default.createTransport({
         host: "smtp.gmail.com",
         port: 587,
-        secure: process.env.NODE_ENV === "production", // true for port 465, false for other ports
+        // secure: process.env.NODE_ENV === "production", // true for port 465, false for other ports
+        secure: false,
         auth: {
             user: process.env.NODE_MAILER_USER,
             pass: process.env.NODE_MAILER_PASSWORD,
         },
+        tls: {
+            rejectUnauthorized: false // Sometimes helps on some hosts
+        }
     });
     const mailData = {
         from: '"DocEye 🩺" <utsho926@gmail.com>', // sender address
--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -10,11 +10,15 @@ export const sendEmail = async (payload: TPayload) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: process.env.NODE_ENV === "production", // true for port 465, false for other ports
+    // secure: process.env.NODE_ENV === "production", // true for port 465, false for other ports
+    secure: false,
     auth: {
       user: process.env.NODE_MAILER_USER,
       pass: process.env.NODE_MAILER_PASSWORD,
     },
+    tls: {
+      rejectUnauthorized: false // Sometimes helps on some hosts
+    }
   });
 
   const mailData = {
